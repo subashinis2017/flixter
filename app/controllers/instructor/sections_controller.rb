ac941596dbcd10fce1b9c1be9bf8db89a69ca8df
Subashini <subashinis2017@gmail.com>
2019-08-23 16:31:56 +0000

    Changes for debugging navbar issue

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::SectionsController < ApplicationController
     before_action :authenticate_user!
-    before_action :require_authorized_for_current_course, only: [:create]
+    before_action :require_authorized_for_current_course, only: [:new, :create]
     before_action :require_authorized_for_current_section, only: [:update]
     
   
